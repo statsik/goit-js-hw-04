252a849f0b0f65490803ae62e2f96a0f5f84dd2c
Daniil Statsenko <daniilstatsenko23@gmail.com>
2024-12-19 23:09:10 +0100

    Commit-1

---
+++ js/task-3.js
@@ -0,0 +1,20 @@
+const profile = {
+    username: "Jacob", 
+    playTime: 300,
+    changeUsername(newName) {
+            this.username = newName;
+    },
+    updatePlayTime(hours) {
+            this.playTime = hours;
+    },
+    getInfo() {
+        const result = `${profile.username} has ${profile.playTime} active hours!`;
+        return result;
+    }
+};
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+profile.updatePlayTime(20);
+console.log(profile.getInfo());
